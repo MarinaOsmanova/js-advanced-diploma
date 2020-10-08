e4b96d1d7f34a15635cdb1cbe545e2aa0f5d189f
Mtaka <mary.mtaka@gmail.com>
2020-10-08 20:20:42 +0500

    up health to 100

--- src/js/__tests__/Bowman.test.js
+++ src/js/__tests__/Bowman.test.js
@@ -6,13 +6,13 @@ test('An instance of the Bowman class is supplied with the correct fields', () =
   expect(character.level).toEqual(2);
   expect(character.attack).toEqual(25);
   expect(character.defence).toEqual(25);
-  expect(character.health).toEqual(50);
+  expect(character.health).toEqual(100);
   expect(character.attackRange).toEqual(2);
   expect(character.moveRange).toEqual(2);
 });
 
 test('infoString returns the correct description', () => {
   const character = new Bowman(2);
-  const expected = '<span class="level"></span>2 <span class="attack"></span>25 <span class="defence"></span>25 <span class="health"></span>50';
+  const expected = '<span class="level"></span>2 <span class="attack"></span>25 <span class="defence"></span>25 <span class="health"></span>100';
   expect(character.infoString()).toEqual(expected);
 });
